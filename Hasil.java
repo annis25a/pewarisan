7fafcaafef4a4701b0921acc2bd52413681d476c
annis25a <100254986+annis25a@users.noreply.github.com>
2022-02-23 11:29:59 +0700

    Add files via upload

---
+++ Hasil.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inheritance;
+
+/**
+ *
+ * @author anisa
+ */
+public class Hasil {
+  
+
+    public static void main(String[] args) {
+        Anak dina=new Anak();
+        dina.Cetak();
+    }
+
+}
+
